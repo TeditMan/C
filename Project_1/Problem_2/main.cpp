fc7942ce9f53c53dddca5bbd2f0925d41b5ec4f6
Yaroslav Frolov <tedit.man@gmail.com>
2022-02-20 20:06:12 +0300

    Project_1, Problem_1 solved

--- Project_1/Problem_2/main.cpp
+++ Project_1/Problem_2/main.cpp
@@ -1,7 +1,33 @@
-#include "iostream"
+#include <iostream>
 
 int main() {
-    std::cout << "Hello, world";
+    std::cout << "Hello, World!" << std::endl;
+    int n, counter_open = 0, counter_close = 0;
+    std::cin >> n;
+    if (n % 2 != 0) {
+        std::cout << "sequence cannot be correct\n";
+    }
+    else {
+        for (int i = 0; i < n; i++) {
+            char k;
+            std::cin >> k;
+            if (k == '(') {
+                counter_open += 1;
+            } else {
+                if (k == ')') {
+                    counter_close += 1;
+                } else {
+                    std::cout << "incorrect input" << '\n';
+                    break;
+                }
+            }
+        }
+        if (counter_open == counter_close) {
+            std::cout << "sequence is correct" << '\n';
+        } else {
+            std::cout << "sequence is incorrect";
+        }
+    }
     return 0;
 }
 
