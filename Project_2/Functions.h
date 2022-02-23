99d080168f53dcd9370f75aae39d9db8ac094674
Yaroslav Frolov <tedit.man@gmail.com>
2022-02-23 14:58:01 +0300

    Working version of project 2

--- Project_2/Functions.h
+++ Project_2/Functions.h
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <random>
 #include <cassert>
-const int n = 10;
+const int n = 1;
 
 struct PRNG {
     std::mt19937 engine;
@@ -13,7 +13,7 @@ void initGenerator(PRNG &generator) {
 }
 
 int random(PRNG &generator, int minValue, int maxValue) {
-    assert(minValue < maxValue);
+    assert(minValue <= maxValue);
     std::uniform_int_distribution<int> distribution(minValue, maxValue);
     return distribution(generator.engine);
 }
@@ -24,8 +24,9 @@ void fill_movement_bool(int (&A)[n][3]) {
     }
 }
 
-void create_config(int (&A)[n][3], int width, int height, PRNG generator) {
-    for (auto & i : A) {
+void create_config(int (&A)[n][3], const int width, const int height, PRNG generator) {
+    initGenerator(generator);
+    for (auto &i : A) {
         i[0] = random(generator, 1, width);
         i[1] = random(generator, 1, height);
     }
@@ -44,26 +45,13 @@ void check(int A[n][3], bool &done) {
 
 void check_movement(int (&A)[n][3], const int width, const int height, int a_o_t_l_s[n][2]) {
     for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (abs(A[i][0] - A[j][0]) + abs(A[i][1] - A[j][1]) == 1) {
-                A[i][2] = 0;
-                A[j][2] = 0;
-            }
-        }
-    }
-    for (auto & i : A) {
-        if (i[0] == 1 or i[0] == width or i[1] == 1 or i[1] == height) {
-            i[2] = 0;
-        }
-    }
-    for (int i = 0; i < n - 1; i++) {
         int temporary_array[3] = {-1, -1, -1}, k = 0;
         for (int j = i + 1; j < n; j++) {
             if (A[i][0] == A[j][0] and A[i][1] == A[j][1]) {
                 temporary_array[k] = j;
                 k += 1;
             }
-            for (int l: temporary_array) {
+            for (int l : temporary_array) {
                 if (l != -1) {
                     A[l][0] -= a_o_t_l_s[l][0];
                     A[l][1] -= a_o_t_l_s[l][0];
@@ -71,12 +59,56 @@ void check_movement(int (&A)[n][3], const int width, const int height, int a_o_t
             }
         }
     }
+    if (width == 1) {
+        if (height == 1) {
+            A[0][2] = 0;
+        }
+        else {
+            for (auto & i : A) {
+                if (i[1] == 1 or i[1] == height) {
+                    i[2] = 0;
+                }
+            }
+        }
+    }
+    else if (height == 1) {
+        for (auto & i : A) {
+            if (i[0] == 1 or i[0] == width) {
+                i[2] = 0;
+            }
+        }
+    }
+    else {
+        for (auto & i : A) {
+            if (i[0] == 1 or i[0] == width or i[1] == 1 or i[1] == height) {
+                i[2] = 0;
+            }
+        }
+    }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if ((abs(A[i][0] - A[j][0]) + abs(A[i][1] - A[j][1])) == 1) {
+                A[i][2] = 0;
+                A[j][2] = 0;
+            }
+        }
+    }
 }
 
-void do_movement(int (&A)[n][3], int a_four[4][2], PRNG generator, int (&a_o_t_l_s)[n][2]) {
+void do_movement(int (&A)[n][3], int a_four[4][2], PRNG generator, int (&a_o_t_l_s)[n][2], const int width, const int height) {
+    initGenerator(generator);
     for (int i = 0; i < n; i++) {
         if (A[i][2] == 1) {
-            int var_int = random(generator, 1, 4);
+            int var_int;
+            if (width == 1) {
+                var_int = random(generator, 2, 3);
+            }
+            else if (height == 1) {
+                var_int = random(generator, 0, 1);
+            }
+            else {
+                var_int = random(generator, 0, 3);
+            }
             A[i][0] += a_four[var_int][0];
             A[i][1] += a_four[var_int][1];
             a_o_t_l_s[i][0] = a_four[var_int][0];
--- Project_2/lab.cpp
+++ Project_2/lab.cpp
@@ -3,15 +3,12 @@
 using namespace std;
 
 int main() {
-    const int width = 10, height = 10, s = width * height;
+    const int width = 100, height = 1, s = width * height;
     int A[n][3], four[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}},
     counter = 0, array_of_the_last_step[n][2];
     bool done = false;
     PRNG generator;
-
     fill_movement_bool(A);
-    initGenerator(generator);
-
     while (!done) {
         done = true;
         create_config(A, width, height, generator);
@@ -19,9 +16,11 @@ int main() {
     }
     while (!check_completeness(A)) {
         check_movement(A, width, height, array_of_the_last_step);
-        do_movement(A, four, generator, array_of_the_last_step);
+        do_movement(A, four, generator, array_of_the_last_step, width, height);
         counter += 1;
     }
-    cout << counter << '\n' << 100 * n / s;
+    double n_d = n, s_d = s, ratio = n_d / s_d;
+    cout << "rules are different for case where width = 1 or height = 1, and where width > 1 and height > 1 \n";
+    cout << "time: " <<  counter << '\n' << "ratio of number of empty cells to area: " << ratio;
     return 0;
 }
